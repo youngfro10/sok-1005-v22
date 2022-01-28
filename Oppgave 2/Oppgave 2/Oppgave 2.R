aa54ae3ddb1778f38bc6ccb134b26bab4ce6bcce
youngfro10 <91133291+youngfro10@users.noreply.github.com>
2022-01-28 16:15:30 +0100

    Update Oppgave 2.R

--- Oppgave 2/Oppgave 2/Oppgave 2.R
+++ Oppgave 2/Oppgave 2/Oppgave 2.R
@@ -8,3 +8,17 @@
 #Finn JSON lenken med JSON dataene som figuren leser data fra.
 #Benytt en pakke i R som leser JSON, og last ned dataene fra lenken.
 #Lag deretter et ggplot som repliserer figuren over.
+
+
+
+
+#Oppgave 2
+
+# Det er en klar negativ sammenheng mellom antall døde per 100 000 (y-aksen) 
+# og andel av befolkningen som er vaksinert (x-aksen).
+
+#Benytt R sin lm() funksjon. Angi variabelen på y aksen og x aksen
+#og spesifiser datasettet.
+#lm(<navn på y-variabel> ~ <navn på x-variabel>, data = <navn på datasettet>)
+#Etter å ha “kjørt” koden, hvordan tolker du de to verdiene på den tilpassa linja?
+#Legg den tilpassede linja til ggplot ved å bruke  + geom_smooth(method = lm).
