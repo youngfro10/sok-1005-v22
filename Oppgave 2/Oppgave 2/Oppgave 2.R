d0ee7761a3d2916cac497f1259bba018f32c1d58
youngfro10 <91133291+youngfro10@users.noreply.github.com>
2022-01-28 16:11:04 +0100

    Create Oppgave 2.R

---
+++ Oppgave 2/Oppgave 2/Oppgave 2.R
@@ -0,0 +1,10 @@
+# Oppgave 1
+
+# For å få tilgang til nettsiden må du registrere deg hos NYT, dette er gratis. 
+# Om du ikke ønsker det, kan du finne html fila i Canvas/Filer.
+# Når nettsiden er lastet i en browser, kan du se på html koden ved å høyreklikke på musen. 
+# Velg vis sidekilde/source.
+
+#Finn JSON lenken med JSON dataene som figuren leser data fra.
+#Benytt en pakke i R som leser JSON, og last ned dataene fra lenken.
+#Lag deretter et ggplot som repliserer figuren over.
